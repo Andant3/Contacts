8bdbc77455296bfc3c85e3a6a55348580e62f73b
Andant3 <kamikadze3571@gmail.com>
2024-04-26 00:25:36 +0200

    ViewModel realisation.

--- app/src/main/java/com/example/contacts/viewmodel/ContactViewModel.kt
+++ app/src/main/java/com/example/contacts/viewmodel/ContactViewModel.kt
@@ -4,8 +4,10 @@ import androidx.databinding.Bindable
 import androidx.databinding.Observable
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.contacts.repository.ContactRepository
 import com.example.contacts.room.ContactModel
+import kotlinx.coroutines.launch
 
 class ContactViewModel(private val repository: ContactRepository) : ViewModel(), Observable {
 
@@ -18,7 +20,7 @@ class ContactViewModel(private val repository: ContactRepository) : ViewModel(),
     val inputName = MutableLiveData<String?>()
 
     @Bindable
-    val inoutEmail = MutableLiveData<String?>()
+    val inputEmail = MutableLiveData<String?>()
 
     @Bindable
     val saveOrUpdateButtonText = MutableLiveData<String>()
@@ -37,6 +39,64 @@ class ContactViewModel(private val repository: ContactRepository) : ViewModel(),
 
     override fun removeOnPropertyChangedCallback(callback: Observable.OnPropertyChangedCallback?) {
         TODO("Not yet implemented")
+    }
+
+    fun insert(contact: ContactModel) = viewModelScope.launch{
+        repository.insert(contact)
+    }
+
+    fun delete(contact: ContactModel) = viewModelScope.launch {
+        repository.delete(contact)
+        resetFields()
+    }
+
+    fun update(contact: ContactModel) = viewModelScope.launch {
+        repository.update(contact)
+        resetFields()
+    }
+
+    fun clearAll() = viewModelScope.launch {
+        repository.deleteAll()
+    }
+
+    fun saveOrDelete(){
+        if(isUpdateOrDelete){
+            contactToUpdateOrDelete.name = inputName.value!!
+            contactToUpdateOrDelete.email = inputEmail.value!!
+            update(contactToUpdateOrDelete)
+        }else{
+            val name = inputName.value!!
+            val email = inputEmail.value!!
+
+            insert(ContactModel(0, name, email))
+            inputName.value = null
+            inputEmail.value = null
+        }
+    }
+
+    fun clearAllOrDelete(){
+        if(isUpdateOrDelete){
+            delete(contactToUpdateOrDelete)
+        }else{
+            clearAll()
+        }
+    }
+
+    fun initUpdateAndDelete(contact: ContactModel){
+        inputName.value = contact.name
+        inputEmail.value = contact.email
+        isUpdateOrDelete = true
+        contactToUpdateOrDelete = contact
+        saveOrUpdateButtonText.value = "Update"
+        clearAllOrDeleteButtonText.value = "Delete"
+    }
+
+    private fun resetFields(){
+        inputName.value = null
+        inputEmail.value = null
+        isUpdateOrDelete = false
+        saveOrUpdateButtonText.value = "Save"
+        clearAllOrDeleteButtonText.value = "Clear All"
     }
 
 }
